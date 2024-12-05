1bc0146c005dadc53daa1f6760b0b5a87d089c5e
Daiki Nakamura <55738391+y-daiki-nakamura@users.noreply.github.com>
2024-12-05 18:15:00 +0000

    fix base url

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,6 +2,7 @@ import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  base: '/y-daiki-nakamura.github.io/',
   root: path.join(__dirname, 'docs'),
   title: 'My Site',
   icon: '/rspress-icon.png',
